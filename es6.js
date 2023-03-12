8f1bfac80be26f42f6483139a014826d0aa924fb
dg1223 <alavi1223@hotmail.com>
2023-03-12 18:13:34 -0400

    es6 basics 2

--- es6.js
+++ es6.js
@@ -13,3 +13,24 @@ console.log(about);
 
 // 2. Arrow function
 const getFiftyFive = () => 55;
+const addSixtyFive = (num) => num + 65;
+const isEven = (x) => x % 2 === 0;
+const addThreeNums = (x, y, z) => x + y + z;
+const doMath = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
+
+// 3. spread operator
+const newNumbers = numbers; // refers to the same memory space
+const newNumbers2 = [...numbers]; // different locaiton in memory
+// create a new array from an older array and add an elemnent
+const currentNumbers = [...numbers, 55];
+
+numbers.push(99);
+numbers.push(99);
+numbers.push(99);
+
+console.log(newNumbers);
+console.log(newNumbers2);
+console.log(currentNumbers);
